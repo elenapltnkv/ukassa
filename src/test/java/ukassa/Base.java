c759f694fbca1a24fd5f4c62b35dbb2263b40c9b
elenapltnkv <elenapltnkv@gmail.com>
2024-04-03 16:00:54 +0300

    change base

--- src/test/java/ukassa/Base.java
+++ src/test/java/ukassa/Base.java
@@ -2,14 +2,13 @@ package ukassa;
 
 import org.junit.jupiter.api.AfterEach;
 
-import java.util.concurrent.TimeUnit;
 
 
 public class Base {
 
     @AfterEach
     void afteEach() throws InterruptedException {
-        TimeUnit.SECONDS.sleep(5000);
+
     }
 
 
